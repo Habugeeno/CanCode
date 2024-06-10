629c3662bcca7ebc21f51bddb0653ce7fc620fdd
Habugeeno <brownanthony748@gmail.com>
2024-06-10 18:44:43 -0400

    Create comp.py

---
+++ Class3/comp.py
@@ -0,0 +1,22 @@
+'''
+
+'''
+
+
+# import pandas as pd  # type: ignore
+
+
+# # My Student Dictionary
+# cooking_class_dict = {'Name' : ['Mike','Moe','Dick'],
+#                        'favorite food' : ['Pizza', 'Ice cream', 'Mango']}
+
+
+# # What does this look like 
+# students_df = pd.DataFrame(cooking_class_dict)
+# # print(students_df)
+
+# # Output
+# students_df.to_excel('output.xlsx', index = False)
+
+
+
