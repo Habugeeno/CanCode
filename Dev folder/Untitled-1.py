72bda4e2c864a699815574e0392185584216bf86
Habugeeno <brownanthony748@gmail.com>
2024-06-12 17:59:55 -0400

    Aera of a triangle

---
+++ Dev folder/Untitled-1.py
@@ -0,0 +1,18 @@
+'''
+Assignment 2 : Area of an triganle 
+'''
+
+# Area of Triangle= 1/2 * base * height
+
+# The varialbes of the triangle
+base = 65 
+
+height = 20
+
+total = 65 * 20 
+
+# The equation with the variables 
+Area = 1/2 * total
+
+# The printed out solved eqaution
+print(f"This is the Area of the triangle {Area}")
