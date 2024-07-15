9e333cf6849dc556ea916b180259e21364e5dcbd
Habugeeno <brownanthony748@gmail.com>
2024-07-14 21:21:27 -0400

    ples

--- Dev folder/Project 1.py
+++ Dev folder/Project 1.py
@@ -1,29 +1,29 @@
 
 
-user_input = input('please type "ready" to login or type "create login": ')
+
 lst1 = []
 lst2 = []
 
-user = 'asap'
-word = 'rocky'
-
-
-for lo in user_input:
+user_input = print('please type "ready" to login or type "create login": ')
+ready = input('ready')
+create = input('create login')
+while True:
     if user_input == 'create login':
-        lst1 = [(item) for item in input("Enter new username : ").split()]
-        lst2 = [item for item in input("Enter new password : ").split()]
+        point1 =input('Enter new username')
+        lst1.append((point1))
+        point2 = input('Enter password: ')
+        lst2.append(input(point2))
         print(f'New user created, {lst1} is your username. And {lst2} is your password')
-    break
 
-for po in user_input:
+
     if user_input == 'ready':
         next = input('please enter username: ')
         text = input('enter pass: ')
-        if next == user and text == word:
+        if next == lst1 and text == lst2:
             print('logged in')
         
-        else: print('Error incorrect username or password. Please try again')
-        break
+    else: print('Error incorrect username or password. Please try again')
+       
 
 
 
---
+++ Dev folder/prla.py
@@ -0,0 +1,19 @@
+
+
+my_list = []
+
+while True:
+
+    my_add = input("Give me numbers and I will add them to the list. Enter 'q' to quit.")
+ 
+    
+    if my_add == 'q':
+        break
+    
+    else:
+        my_list.append(my_add)
+
+    if my_list == 'correct':
+        print('piss')
+        
+print(my_list)
